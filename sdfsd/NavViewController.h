75ffc8652b4d48b4100cdd314e7daa28fd2820c9
AtMyCode <zhanglijian2013@163.com>
2015-08-11 10:35:15 +0800

    全屏手势返回
    全屏手势返回

---
+++ sdfsd/NavViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavViewController.h
+//  sdfsd
+//
+//  Created by ss on 15/8/11.
+//  Copyright (c) 2015年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavViewController : UINavigationController
+
+@end
---
+++ sdfsd/NavViewController.m
@@ -0,0 +1,53 @@
+//
+//  NavViewController.m
+//  sdfsd
+//
+//  Created by ss on 15/8/11.
+//  Copyright (c) 2015年 ss. All rights reserved.
+//
+
+#import "NavViewController.h"
+
+@interface NavViewController ()<UIGestureRecognizerDelegate>
+
+@end
+
+@implementation NavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    id target = self.interactivePopGestureRecognizer.delegate;
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:target action:@selector(handleNavigationTransition:)];
+    
+    pan.delegate = self;
+    
+    [self.view addGestureRecognizer:pan];
+    
+    self.interactivePopGestureRecognizer.enabled = NO;
+}
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    if (self.childViewControllers.count == 1) {
+        return NO;
+    }
+    return YES;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
